2e25543b4001bd3471a193ce1323dc54066a1328
Bhavin1313 <99348404+Bhavin1313@users.noreply.github.com>
2023-05-24 10:15:12 -0700

    OOP Architecture

---
+++ OOP_Architecture_1.dart
@@ -0,0 +1,31 @@
+/1. WAP to take a list using named parameter and return sum of all elements of list./
+import 'dart:io';
+
+class Sum {
+  List<int> l = [];
+
+  void setData() {
+    stdout.write("Enter length of list: ");
+    int n = int.parse(stdin.readLineSync()!);
+    print("Enter elements:");
+    for (int i = 0; i < n; i++) {
+      l.add(int.parse(stdin.readLineSync()!));
+    }
+    print("${l}");
+  }
+
+  void calculateSum() {
+    int sum = 0;
+    for (int i = 0; i < l.length; i++) {
+      sum += l[i];
+    }
+    print("Sum of list is: $sum");
+    ;
+  }
+}
+
+void main() {
+  Sum sum = Sum();
+  sum.setData();
+  sum.calculateSum();
+}
